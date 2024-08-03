d72ceb3f4b0b5d486a8018ddced2170d681a4d2a
Luthfi Yufajjiru <luthfiyufajjiru@gmail.com>
2024-08-03 09:52:39 +0700

    feat: define config type

---
+++ pkg/adapters/config.go
@@ -0,0 +1,26 @@
+package adapters
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+type (
+	Config map[string]string
+)
+
+func (c *Config) Load(filePath string) {
+	mp, err := godotenv.Read(filePath)
+	if err != nil {
+		fmt.Println("load from host env")
+		mp = make(map[string]string)
+		for _, e := range os.Environ() {
+			pair := strings.SplitN(e, "=", 2)
+			mp[pair[0]] = pair[1]
+		}
+	}
+	*c = mp
+}

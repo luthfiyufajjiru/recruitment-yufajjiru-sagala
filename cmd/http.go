e2e3e877fb33a2ec4f7169b57b21cbaaf14c32a9
Luthfi Yufajjiru <luthfiyufajjiru@gmail.com>
2024-08-03 11:08:45 +0700

    feat: router

--- cmd/http.go
+++ cmd/http.go
@@ -2,15 +2,16 @@ package cmd
 
 import (
 	"net/http"
+	"sagala-todo/dependency"
 )
 
 func InitHttpServer() {
 	mux := http.NewServeMux()
 
-	// v1Todo := dependency.InitTodoV1HttpHandler()
+	v1Todo := dependency.InitTodoV1HttpHandler()
 
-	// mux.Handle("/v1", v1Todo.RootHandler())
-	// mux.Handle("/v1/{id}", v1Todo.TaskDetail())
+	mux.Handle("/v1", v1Todo.RootHandler())
+	mux.Handle("/v1/{id}", v1Todo.TaskDetail())
 
 	http.ListenAndServe("", mux)
 }

dd327b6e5b00330a874bd95a34f8f8cc56d29497
Luthfi Yufajjiru <luthfiyufajjiru@gmail.com>
2024-08-03 12:41:09 +0700

    feat: define logger TaskDetail

--- src/delivery/v1http/todo.go
+++ src/delivery/v1http/todo.go
@@ -2,7 +2,9 @@ package v1http
 
 import (
 	"net/http"
+	"sagala-todo/pkg/constants"
 	"sagala-todo/src/delivery"
+	"github.com/sirupsen/logrus"
 )
 
 func (h *V1Handler) RootHandler() http.HandlerFunc {
@@ -18,6 +20,10 @@ func (h *V1Handler) RootHandler() http.HandlerFunc {
 }
 
 func (h *V1Handler) TaskDetail() http.HandlerFunc {
+	const handlerName = "TaskDetail"
+	logger := logger.WithFields(logrus.Fields{
+		constants.LFKHandlerName: handlerName,
+	})
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:

11cd7c57600c07f577308aafb6621bcb6c821547
Luthfi Yufajjiru <luthfiyufajjiru@gmail.com>
2024-08-03 12:35:51 +0700

    feat: handle unhandled error

--- src/delivery/common_http.go
+++ src/delivery/common_http.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"sagala-todo/pkg/constants"
+
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -14,4 +17,12 @@ func HandleUnknownHttpMethod(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusNotFound)
 	w.Header().Set(constants.HeaderKeyContentType, constants.HeaderTextPlain)
 	fmt.Fprint(w, UnknownHttpMethod)
+}
+
+func HandleUnhandledError(w http.ResponseWriter, err error, logger *logrus.Entry) {
+	errorId := uuid.NewString()
+	logger.WithField(constants.LFKErrorId, errorId).Error(err)
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Header().Set(constants.HeaderKeyContentType, constants.HeaderTextPlain)
+	fmt.Fprintf(w, "unhandled error, error id: %s", errorId)
 }

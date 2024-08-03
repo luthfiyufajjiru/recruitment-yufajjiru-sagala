fc5a5d2bfef8d926b4cc290d4405817bf4dae3b3
Luthfi Yufajjiru <luthfiyufajjiru@gmail.com>
2024-08-03 11:20:13 +0700

    chore: skeleton todo handler

--- src/delivery/v1http/todo.go
+++ src/delivery/v1http/todo.go
@@ -1,13 +1,31 @@
 package v1http
 
-import "net/http"
+import (
+	"net/http"
+	"sagala-todo/src/delivery"
+)
 
 func (h *V1Handler) RootHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+		case http.MethodGet:
+		default:
+			delivery.HandleUnknownHttpMethod(w)
+			return
+		}
 	}
 }
 
 func (h *V1Handler) TaskDetail() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+		case http.MethodPut:
+		case http.MethodDelete:
+		default:
+			delivery.HandleUnknownHttpMethod(w)
+			return
+		}
 	}
 }

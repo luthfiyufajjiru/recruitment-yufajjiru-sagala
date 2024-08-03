53ca78ba5d384a5031dad36d136ce285a4e941de
Luthfi Yufajjiru <luthfiyufajjiru@gmail.com>
2024-08-03 12:41:25 +0700

    feat: handle get detail

--- src/delivery/v1http/todo.go
+++ src/delivery/v1http/todo.go
@@ -1,9 +1,16 @@
 package v1http
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"sagala-todo/pkg/common"
 	"sagala-todo/pkg/constants"
+	customerror "sagala-todo/pkg/custom-error"
 	"sagala-todo/src/delivery"
+	"sagala-todo/src/model"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -25,8 +32,55 @@ func (h *V1Handler) TaskDetail() http.HandlerFunc {
 		constants.LFKHandlerName: handlerName,
 	})
 	return func(w http.ResponseWriter, r *http.Request) {
+		var msgError *customerror.HttpError
+		fn := new(common.LeastError)
+
+		id := r.PathValue("id")
+		if id == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, constants.ErrMsgEmptyId)
+			return
+		}
+
 		switch r.Method {
 		case http.MethodGet:
+			var record model.TaskPresenter
+			var mRecord []byte
+
+			fn.Do(func() (err error) {
+				record, err = h.Useacse.GetTask(id)
+				return
+			})
+
+			fn.Do(func() (err error) {
+				mRecord, err = json.Marshal(record)
+				if err != nil {
+					logger.Error(err)
+					err = &customerror.HttpError{
+						Message:    constants.ErrMsgFailedSerializeRecord,
+						Err:        err,
+						StatusCode: http.StatusInternalServerError,
+					}
+				}
+				return
+			})
+
+			err := fn.Err()
+			asMsgError := errors.As(err, &msgError)
+
+			if asMsgError {
+				w.WriteHeader(msgError.StatusCode)
+				fmt.Fprintf(w, msgError.Message)
+				return
+			} else if err != nil {
+				delivery.HandleUnhandledError(w, err, logger)
+				return
+			}
+
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set(constants.HeaderKeyContentType, constants.HeaderApplicationJSON)
+			fmt.Fprint(w, string(mRecord))
+			return
 		case http.MethodPut:
 		case http.MethodDelete:
 		default:

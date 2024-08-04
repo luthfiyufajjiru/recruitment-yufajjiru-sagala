59dda9dd96c0e39753ab660e27ffeae43f4e2255
Luthfi Yufajjiru <luthfiyufajjiru@gmail.com>
2024-08-04 09:28:43 +0700

    chore: add error panic index

--- pkg/constants/errors.go
+++ pkg/constants/errors.go
@@ -1,6 +1,12 @@
 package constants
 
+import "errors"
+
 const (
 	ErrMsgFailedSerializeRecord = "failed to serialized record"
 	ErrMsgEmptyId               = "id could not be empty"
+)
+
+var (
+	ErrPanic = errors.New("panic occurs")
 )

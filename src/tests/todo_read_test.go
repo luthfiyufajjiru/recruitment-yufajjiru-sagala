f72f91b0c941094d23706b61635709b5299716fa
Luthfi Yufajjiru <luthfiyufajjiru@gmail.com>
2024-08-04 10:08:43 +0700

    feat: unit test get task list

--- src/tests/todo_read_test.go
+++ src/tests/todo_read_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/stretchr/testify/assert"
+	"github.com/c2fo/testify/assert"
 )
 
 var returnedCols = []string{"id", "content", "status", "created_at", "created_by", "updated_at", "updated_by", "deleted_at"}
@@ -69,5 +69,155 @@ func TestGetTaskDetail(t *testing.T) {
 	err := mockDb.ExpectationsWereMet()
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestGetTasks(t *testing.T) {
+	type expectation struct {
+		limit, offset    *int
+		search, status   *string
+		prepareStatement func(mdb sqlmock.Sqlmock)
+		assertFn         func(t *testing.T, mdb sqlmock.Sqlmock, err error)
+	}
+
+	cfg := dependency.InitConfiguration()
+
+	usecase := dependency.InitTodoUsecaseMock(cfg)
+
+	mockDb := usecase.Sql[constants.ConnSqlDefault].MockCtrl
+
+	var (
+		commonStr    = "somestr"
+		commonStatus = "Done"
+		commonInt    = 1
+		commonError  = errors.New(commonStr)
+	)
+
+	expectations := []expectation{
+		{
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				assert.True(t, errors.Is(err, constants.ErrPanic))
+			},
+		},
+		{
+			status: &commonStr,
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				var msgError *customerror.HttpError
+				ok := errors.As(err, &msgError)
+				assert.True(t, ok)
+				assert.Equal(t, http.StatusBadRequest, msgError.StatusCode)
+			},
+		},
+		{
+			status: &commonStatus,
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				assert.True(t, errors.Is(err, constants.ErrPanic))
+			},
+		},
+		{
+			limit: &commonInt,
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				assert.True(t, errors.Is(err, constants.ErrPanic))
+			},
+		},
+		{
+			offset: &commonInt,
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				assert.True(t, errors.Is(err, constants.ErrPanic))
+			},
+		},
+		{
+			limit:  &commonInt,
+			offset: &commonInt,
+			prepareStatement: func(mdb sqlmock.Sqlmock) {
+				mdb.ExpectQuery("SELECT (.+) FROM tasks WHERE (.+) ORDER BY (.+) LIMIT (.+) OFFSET (.+)").WillReturnError(commonError)
+			},
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				assert.True(t, errors.Is(err, commonError))
+				assert.Nil(t, mdb.ExpectationsWereMet())
+			},
+		},
+		{
+			limit:  &commonInt,
+			offset: &commonInt,
+			prepareStatement: func(mdb sqlmock.Sqlmock) {
+				mdb.ExpectQuery("SELECT (.+) FROM tasks WHERE (.+) ORDER BY (.+) LIMIT (.+) OFFSET (.+)").WillReturnRows(sqlmock.NewRows(returnedCols).AddRow(commonStr, commonStr, commonStr, 1, nil, 1, nil, 0))
+				mdb.ExpectQuery("SELECT (.+) FROM tasks WHERE (.+) ORDER BY (.+)").WillReturnError(commonError)
+			},
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				assert.True(t, errors.Is(err, commonError))
+				assert.Nil(t, mdb.ExpectationsWereMet())
+			},
+		},
+		{
+			limit:  &commonInt,
+			offset: &commonInt,
+			status: &commonStatus,
+			prepareStatement: func(mdb sqlmock.Sqlmock) {
+				mdb.ExpectQuery("SELECT (.+) FROM tasks WHERE (.+) AND (.+) ORDER BY (.+) LIMIT (.+) OFFSET (.+)").WillReturnRows(sqlmock.NewRows(returnedCols).AddRow(commonStr, commonStr, commonStr, 1, nil, 1, nil, 0))
+				mdb.ExpectQuery("SELECT (.+) FROM tasks WHERE (.+) AND (.+) ORDER BY (.+)").WillReturnError(commonError)
+			},
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				assert.True(t, errors.Is(err, commonError))
+				assert.Nil(t, mdb.ExpectationsWereMet())
+			},
+		},
+		{
+			limit:  &commonInt,
+			offset: &commonInt,
+			status: &commonStatus,
+			search: &commonStr,
+			prepareStatement: func(mdb sqlmock.Sqlmock) {
+				mdb.ExpectQuery("SELECT (.+) FROM tasks WHERE (.+) AND (.+) AND (.+) ORDER BY (.+) LIMIT (.+) OFFSET (.+)").WillReturnRows(sqlmock.NewRows(returnedCols).AddRow(commonStr, commonStr, commonStr, 1, nil, 1, nil, 0))
+				mdb.ExpectQuery("SELECT (.+) FROM tasks WHERE (.+) AND (.+) AND (.+) ORDER BY (.+)").WillReturnError(commonError)
+			},
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				assert.True(t, errors.Is(err, commonError))
+				assert.Nil(t, mdb.ExpectationsWereMet())
+			},
+		},
+		{
+			limit:  &commonInt,
+			offset: &commonInt,
+			status: &commonStatus,
+			search: &commonStr,
+			prepareStatement: func(mdb sqlmock.Sqlmock) {
+				mdb.ExpectQuery("SELECT (.+) FROM tasks WHERE (.+) AND (.+) AND (.+) ORDER BY (.+)").WillReturnError(commonError)
+			},
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				assert.True(t, errors.Is(err, commonError))
+				assert.Nil(t, mdb.ExpectationsWereMet())
+			},
+		},
+		{
+			limit:  &commonInt,
+			offset: &commonInt,
+			status: &commonStatus,
+			search: &commonStr,
+			prepareStatement: func(mdb sqlmock.Sqlmock) {
+				mdb.ExpectQuery("SELECT (.+) FROM tasks WHERE (.+) AND (.+) AND (.+) ORDER BY (.+) LIMIT (.+) OFFSET (.+)").WillReturnRows(sqlmock.NewRows(returnedCols).AddRow(commonStr, commonStr, commonStr, 1, nil, 1, nil, 0))
+				mdb.ExpectQuery("SELECT (.+) FROM tasks WHERE (.+) AND (.+) AND (.+) ORDER BY (.+)").WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
+
+			},
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				assert.Nil(t, err)
+				assert.Nil(t, mdb.ExpectationsWereMet())
+			},
+		},
+	}
+
+	for _, expectation := range expectations {
+		if expectation.limit != nil && expectation.offset != nil {
+			expectation.prepareStatement(mockDb)
+		}
+
+		_, _, err := usecase.GetTasks(expectation.limit, expectation.offset, expectation.search, expectation.status)
+
+		expectation.assertFn(t, mockDb, err)
+	}
+
+	err := mockDb.ExpectationsWereMet()
+	if err != nil {
+		t.Fatal(err)
 	}
 }

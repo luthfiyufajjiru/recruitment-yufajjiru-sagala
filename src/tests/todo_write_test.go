45bfe68d7df59724d94f4db554019a84b4a3c106
Luthfi Yufajjiru <luthfiyufajjiru@gmail.com>
2024-08-04 10:47:50 +0700

    feat: unit test update

--- src/tests/todo_write_test.go
+++ src/tests/todo_write_test.go
@@ -111,3 +111,151 @@ func TestPost(t *testing.T) {
 		expectation.assertFn(t, mockDb, err)
 	}
 }
+
+func TestUpdate(t *testing.T) {
+	type expectation struct {
+		payload          model.TaskDTO
+		prepareStatement func(mdb sqlmock.Sqlmock)
+		assertFn         func(t *testing.T, mdb sqlmock.Sqlmock, err error)
+	}
+
+	cfg := dependency.InitConfiguration()
+
+	usecase := dependency.InitTodoUsecaseMock(cfg)
+
+	mockDb := usecase.Sql[constants.ConnSqlDefault].MockCtrl
+
+	query := "UPDATE tasks SET (.+) WHERE id = (.+) AND \\(deleted_at is null OR deleted_at = 0\\) returning id"
+
+	expectations := []expectation{
+		{
+			payload: model.TaskDTO{},
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				var msgErr *customerror.HttpError
+				ok := errors.As(err, &msgErr)
+				assert.True(t, ok)
+				assert.Equal(t, http.StatusBadRequest, msgErr.StatusCode)
+			},
+		},
+		{
+			payload: model.TaskDTO{
+				Content: nullable.NullString{
+					NullString: sql.NullString{
+						String: "hello",
+						Valid:  true,
+					},
+				},
+			},
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				var msgErr *customerror.HttpError
+				ok := errors.As(err, &msgErr)
+				assert.True(t, ok)
+				assert.Equal(t, http.StatusBadRequest, msgErr.StatusCode)
+			},
+		},
+		{
+			payload: model.TaskDTO{
+				Content: nullable.NullString{
+					NullString: sql.NullString{
+						String: "hello",
+						Valid:  true,
+					},
+				},
+				Status: nullable.NullString{
+					NullString: sql.NullString{
+						String: "hello",
+						Valid:  true,
+					},
+				},
+			},
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				var msgErr *customerror.HttpError
+				ok := errors.As(err, &msgErr)
+				assert.True(t, ok)
+				assert.Equal(t, http.StatusBadRequest, msgErr.StatusCode)
+			},
+		},
+		{
+			payload: model.TaskDTO{
+				Content: nullable.NullString{
+					NullString: sql.NullString{
+						String: "hello",
+						Valid:  true,
+					},
+				},
+				Status: nullable.NullString{
+					NullString: sql.NullString{
+						String: "Done",
+						Valid:  true,
+					},
+				},
+			},
+			prepareStatement: func(mdb sqlmock.Sqlmock) {
+				mdb.ExpectQuery(query).WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow("some_id"))
+			},
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				assert.Nil(t, err)
+				assert.Nil(t, mdb.ExpectationsWereMet())
+			},
+		},
+		{
+			payload: model.TaskDTO{
+				Content: nullable.NullString{
+					NullString: sql.NullString{
+						String: "hello",
+						Valid:  true,
+					},
+				},
+				Status: nullable.NullString{
+					NullString: sql.NullString{
+						String: "Done",
+						Valid:  true,
+					},
+				},
+			},
+			prepareStatement: func(mdb sqlmock.Sqlmock) {
+				mdb.ExpectQuery(query).WillReturnError(errors.New("some error"))
+			},
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				assert.NotNil(t, err)
+				assert.Nil(t, mdb.ExpectationsWereMet())
+			},
+		},
+		{
+			payload: model.TaskDTO{
+				Content: nullable.NullString{
+					NullString: sql.NullString{
+						String: "hello",
+						Valid:  true,
+					},
+				},
+				Status: nullable.NullString{
+					NullString: sql.NullString{
+						String: "Done",
+						Valid:  true,
+					},
+				},
+			},
+			prepareStatement: func(mdb sqlmock.Sqlmock) {
+				mdb.ExpectQuery(query).WillReturnError(sql.ErrNoRows)
+			},
+			assertFn: func(t *testing.T, mdb sqlmock.Sqlmock, err error) {
+				var msgErr *customerror.HttpError
+				ok := errors.As(err, &msgErr)
+				assert.True(t, ok)
+				assert.Equal(t, http.StatusNotFound, msgErr.StatusCode)
+				assert.Nil(t, mdb.ExpectationsWereMet())
+			},
+		},
+	}
+
+	for _, expectation := range expectations {
+		if expectation.prepareStatement != nil {
+			expectation.prepareStatement(mockDb)
+		}
+
+		err := usecase.UpdateTask("some_id", expectation.payload)
+
+		expectation.assertFn(t, mockDb, err)
+	}
+}
